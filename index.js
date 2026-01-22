f48adf56b82ba73a205bb6f5390b719b9bcde54e
raffaelebaiocco06-ux <raffaele.baiocco06@gmail.com>
2026-01-22 16:57:21 +0100

    hide

--- index.js
+++ index.js
@@ -4,38 +4,46 @@
 
 const url = "https://api.pexels.com/v1/search?query=kittens";
 const url2 = "https://api.pexels.com/v1/search?query=tiger";
-
-fetch(url, {
-  headers: {
-    Authorization: "1BDDNudRFfFzRVGs6PRkyTa7CquV02OGxdl7CDLciy51yynZfVnnuh5m",
-  },
-})
-  .then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      throw new Error("errore");
-    }
+const getevents = function () {
+  fetch(url, {
+    headers: {
+      Authorization: "1BDDNudRFfFzRVGs6PRkyTa7CquV02OGxdl7CDLciy51yynZfVnnuh5m",
+    },
   })
-  .then((array) => {
-    console.log("array buono ", array);
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error("errore");
+      }
+    })
+    .then((array) => {
+      console.log("array buono ", array);
 
-    const btnload = document.getElementById("Load");
-    const data = array.photos;
-    console.log("array data ", data);
-    const cardimg = document.querySelectorAll(".card img");
-    btnload.addEventListener("click", function () {
-      cardimg.forEach((immagine, conta) => {
-        immagine.src = data[conta].src.original;
+      const btnload = document.getElementById("Load");
+      const data = array.photos;
+      console.log("array data ", data);
+      const cardimg = document.querySelectorAll(".card img");
+      btnload.addEventListener("click", function () {
+        cardimg.forEach((immagine, conta) => {
+          immagine.src = data[conta].src.original;
+        });
       });
-    });
-    const secondbtnload = document.getElementById("secondload");
-    secondbtnload.addEventListener("click", function () {
-      cardimg.forEach((immagine, conta) => {
-        immagine.src = data[conta].src.original; //qua non va
+      const secondbtnload = document.getElementById("secondload");
+      secondbtnload.addEventListener("click", function () {
+        cardimg.forEach((immagine, conta) => {
+          immagine.src = data[conta].src.original; //qua non va
+        });
       });
+      const btnhide = document.querySelectorAll(".HIDE");
+      btnhide.forEach((btn) => {
+        btn.addEventListener("click", () => {
+          btn.closest(".col-md-4").classList.add("d-none");
+        });
+      });
+    })
+    .catch((error) => {
+      console.log("Errore ", error);
     });
-  })
-  .catch((error) => {
-    console.log("Errore ", error);
-  });
+};
+getevents();
